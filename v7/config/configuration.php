b9681332eb2c240e197282481293af5597d2c342
solal gendrin <adresse3105@hotmail.fr>
2017-10-25 13:32:58 +0200

    version apres prototypage

--- v7/config/configuration.php
+++ v7/config/configuration.php
@@ -16,7 +16,6 @@ define('PATH_LIB','./lib/');
 define('PATH_MODELS','./models/');
 define('PATH_VIEWS','./views/');
 define('PATH_SCRIPTS','./scripts/');
-define('PATH_TEXTES','./languages/');
 
 //sous dossiers
 define('PATH_CSS', PATH_ASSETS.'css/');
--- v7/views/index.php
+++ v7/views/index.php
@@ -3,39 +3,21 @@
 
 <!--  Début de la page -->
 <body>
-
-	<div id="colonne_gauche">
-	<img src="<?= PATH_ASSETS ?>images/logo.png" id="logo">
-<!--  Sliders -->
-	<form method="post">
-		<br>
-		<label> <?= CRITERE_PERF ?>
-			<br>
-			<input type="range" name="performance" min="0" max="10" value="<?php echo $performance; ?>
-">
-						
-		</label>
-		<br>
-		<label> <?= CRITERE_AUTONOMIE ?>
-			<br>
-			<input type="range" name="Batterie" min="0" max="10" value="<?php echo $batterie; ?>">
-		</label>
-		<br>
-            <label> <?= CRITERE_PHOTO ?>
-			<br>
-			<input type="range" name="photo" min="0" max="10" value="<?php echo $photo; ?>">
-		</label>
-		<br>
-		<label> <?= CRITERE_BUDGET ?>
-			<br>
-			<input type="number" name="budget" min="0" max="1500">
-		</label>
-		<br>
-		<input type="submit" value= <?= BOUTON_SEARCH ?>>
-		</form>
+	<div id="cadre">
+		<div id="contenu">
+			<img src="assets\images\logo_grand_transparent.png" id="logo">
+			<h1>Que doit être le point fort de votre téléphone ?</h1>
+			<a href="#" class="btn_secondaire">la photographie</a><br>
+			<a href="#" class="btn_secondaire">l'autonomie</a><br>
+			<a href="#" class="btn_secondaire">les performances</a>
+			<div id="trait"></div>
+			<a href="#" class="btn_principal">Recherche personnalisée</a>
+		</div>
 	</div>
+
+
 <!--  Fin de la page -->
 
-
+</body>
 <!--  Pied de page -->
 <?php require_once(PATH_VIEWS.'footer.php'); 
