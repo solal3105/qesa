57f1c904843970cf1caec1b86f791a4e24831e79
solal gendrin <adresse3105@hotmail.fr>
2017-10-25 13:23:25 +0200

    modification ombrage

--- v7/views/index.php
+++ v7/views/index.php
@@ -7,9 +7,9 @@
 		<div id="contenu">
 			<img src="assets\images\logo_grand_transparent.png" id="logo">
 			<h1>Que doit être le point fort de votre téléphone ?</h1>
-			<a href="#" class="btn_secondaire">la photographie</a>
-			<a href="#" class="btn_secondaire">l'autonomie</a>
-			<a href="#" class="btn_secondaire">les performances
+			<a href="#" class="btn_secondaire">la photographie</a><br>
+			<a href="#" class="btn_secondaire">l'autonomie</a><br>
+			<a href="#" class="btn_secondaire">les performances</a>
 			<div id="trait"></div>
 			<a href="#" class="btn_principal">Recherche personnalisée</a>
 		</div>
