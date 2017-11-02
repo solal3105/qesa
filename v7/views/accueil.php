536d16f8d83689e7aca8b0118e43cd3741d46c72
solal gendrin <adresse3105@hotmail.fr>
2017-11-02 13:05:29 +0100

    developement de la page persona

---
+++ v7/views/accueil.php
@@ -0,0 +1,27 @@
+<?php require_once(PATH_VIEWS.'header.php');?>
+	<link href="<?= PATH_CSS ?>accueil.css" rel="stylesheet">
+</head> 
+
+<!--  Début de la page -->
+<body>
+	<div id="cadre">
+		<div id="contenu">
+			<img src="assets\images\logo_grand_transparent.png" id="logo">
+			<h1>Que doit être le point fort de votre téléphone ?</h1>
+			<form method="post" action="?page=personas" id="formulaire">
+				<input type="button" class="btn_secondaire" value="la photographie"  name="photographie" onclick="subForm(1);"><br>
+				<input type="button" class="btn_secondaire" value="l'autonomie" name="autonomie" onclick="subForm(2);"><br>
+				<input type="button" class="btn_secondaire" value="performance" name="performance" onclick="subForm(3);">
+				<div id="trait"></div>
+				<input type="button" value="Recherche personnalisée" name="personnalise" class="btn_principal" onclick="subForm(4);">
+				<input type="hidden" name="persona" value="<?= $_POST['persona'] ?>" id="persona">
+			</form>
+		</div>
+	</div>
+
+
+<!--  Fin de la page -->
+
+</body>
+<!--  Pied de page -->
+<?php require_once(PATH_VIEWS.'footer.php'); 
