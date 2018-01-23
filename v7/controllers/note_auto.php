2f03af68dcff4dcc6707d1253c24902532499b02
lexskiies <alexis.cheyy@gmail.com>
2018-01-23 12:54:24 +0100

    fix note batterie auto

--- v7/controllers/note_auto.php
+++ v7/controllers/note_auto.php
@@ -3,9 +3,11 @@
 require_once(PATH_MODELS."noteDAO.php");
 
 $notes_auto = new noteDAO(1);
-$deciles = $notes_auto->calculDeciles();
+$deciles = $notes_auto->calculDecilesBatt();
 $note_batterie = $notes_auto->calculAllNotesBatterie($deciles);
+//$ram = $notes_auto->calculDecilesRam();
 if ($notes_auto->uptadeNotesBatteries($note_batterie)) {
 	header("Location: ?page=admin");
 	exit;
 }
+$notes_auto->is_batt_existing(17);
